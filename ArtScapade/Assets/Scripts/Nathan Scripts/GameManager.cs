f99f5f2fdc2f5fdffe3ad5e76dfe1ed7df52c41e
Timmir0 <124453971+Timmir0@users.noreply.github.com>
2023-10-01 18:25:43 -0400

    GameManager
    I got a basic GameManager script going. Right now it's only really looking to find out what Camera is active so that info can be given to other pieces. It will also be what keeps track of player time and other statistics as well.

---
+++ ArtScapade/Assets/Scripts/Nathan Scripts/GameManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] GameObject activeCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public GameObject GetActiveCamera()
+    {
+        return activeCamera;
+    }
+    
+    public void SetActiveCamera(GameObject nActiveCamera)
+    {
+        activeCamera = nActiveCamera;
+    }
+
+}
