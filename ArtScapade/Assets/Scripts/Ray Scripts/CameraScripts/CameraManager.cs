30022121df5d5b376f93807a53046c12195d91f0
Ray Sebastian <raysebastian38@yahoo.com>
2023-10-29 15:00:04 -0400

    Updated the camera switch
    Instead of numbers its the arrow keys and A and D

---
+++ ArtScapade/Assets/Scripts/Ray Scripts/CameraScripts/CameraManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+
+    public Camera[] allCameras;
+
+    public int currentCamera;
+
+    public void Awake()
+    {
+
+        for (var i = 1; i < allCameras.Length; ++i)
+            allCameras[i].enabled = false;
+    } 
+    
+    void IncCamera()
+    {
+        allCameras[currentCamera].enabled = false;
+        currentCamera = ++currentCamera % allCameras.Length;
+        allCameras[currentCamera].enabled = true;
+    }
+
+    void DecCamera()
+    {
+        allCameras[currentCamera].enabled = false;
+        currentCamera = --currentCamera < 0 ? allCameras.Length - 1 : currentCamera;
+        allCameras[currentCamera].enabled = true;
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            IncCamera();
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            DecCamera();
+
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            IncCamera();
+        }
+        
+       
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            DecCamera();
+        }
+    }
+}
