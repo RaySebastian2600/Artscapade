e1351e8564b00fa205ee9b4efc87754bac03ced1
Timmir0 <124453971+Timmir0@users.noreply.github.com>
2023-11-06 17:11:21 -0500

    Another one

--- ArtScapade/Assets/Scripts/Nathan Scripts/MinimapControls.cs
+++ ArtScapade/Assets/Scripts/Nathan Scripts/MinimapControls.cs
@@ -1,19 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MinimapControls : MonoBehaviour
 {
+    [SerializeField] Sprite cameraSpriteOff;
+    [SerializeField] Sprite cameraSpriteOn;
+    [SerializeField] Sprite droneSpriteOff;
+    [SerializeField] Sprite droneSpriteOn;
+
+    [SerializeField] GameManager gameManager;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = FindObjectOfType<GameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GetInput();
+    }
+
+    private void GetInput()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit rh;
+
+            if (Physics.Raycast(ray, out rh))
+            {
+                if (rh.collider.CompareTag("Clickable"))
+                {
+                    //Find out if it's a camera or a drone
+                    //use the method that object belongs to
+                }
+            }
+        }
     }
 
     public void ClickCamera(int cam)
@@ -24,6 +51,6 @@ public class MinimapControls : MonoBehaviour
 
     public void ClickDrone(int drone)
     {
-        //clicks on a drone to activate it
+        //Switches the UI to show Drone stuff when activated
     }
 }
